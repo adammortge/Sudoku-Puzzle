952c5c9a93eea011c143bf550b08007c679923ba
Adam Mortge 2017 <gocubs99@comcast.net>
2015-05-12 09:51:44 -0500

    For main

---
+++ Sudoku Generator/src/sudoku/generator/SudokuGenerator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudoku.generator;
+
+/**
+ *
+ * @author amortge7
+ * version: 1
+ */
+public class SudokuGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+    
+}
